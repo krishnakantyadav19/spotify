e3f0d38d462a018c0bc781c4000b4a37dbfaa2d6
krishnakantyadav19 <krishnakantyadav19@gmail.com>
2023-03-27 14:35:23 +0530

    3rd Commit

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode } from "swiper";
-import PlayPause from "./playPause";
+import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 import "swiper/css";
